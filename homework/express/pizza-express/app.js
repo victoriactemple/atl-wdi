e0089803031c5d6c3003cc352a3a6f813ede9417
victoriactemple <victoriactemple@gmail.com>
2017-09-19 17:27:46 -0400

    completed GET routes

--- homework/express/pizza-express/app.js
+++ homework/express/pizza-express/app.js
@@ -5,6 +5,22 @@ var app     = express();
 // assigning 3000 as our port
 var port    = 3000;
 
+app.get('/', (req, res) =>{
+    res.send("Welcome to Pizza Express");
+})
+
+app.get('/topping/:type', (req, res, next) =>{
+    const type = req.params.type
+   res.send(`${type} a good choice!`);
+
+});
+
+app.get('/order/:amount/:size', (req, res, next) =>{
+    const amount = req.params.amount;
+    const size = req.params.size;
+    res.send(`Your order of ${amount} ${size} will be reading in 1 minute!`)
+})
+
 // tells the server to listen for requests on port 3000
 app.listen(port, function(){
   console.log("==========================")
