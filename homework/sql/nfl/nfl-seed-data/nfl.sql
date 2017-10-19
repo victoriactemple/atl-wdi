658688985ff095f5a2a0a321230a464322327956
victoriactemple <victoriactemple@gmail.com>
2017-10-19 18:49:29 -0400

    NFL - wow there's a lot of nfl data

--- homework/sql/nfl/nfl-seed-data/nfl.sql
+++ homework/sql/nfl/nfl-seed-data/nfl.sql
@@ -98,3 +98,55 @@ nfl=# SELECT COUNT (*) FROM players;
 (1 row)
 
 #5
+
+SELECT name, head_coach FROM teams WHERE conference = 'NFC' AND division = 'North' OR conference = 'AFC' AND division = 'East';
+         name         |   head_coach
+----------------------+----------------
+ Buffalo Bills        | Doug Marrone
+ Miami Dolphins       | Joe Philbin
+ New England Patriots | Bill Belichick
+ New York Jets        | Rex Ryan
+ Chicago Bears        | Marc Trestman
+ Detroit Lions        | Jim Caldwell
+ Green Bay Packers    | Mike McCarthy
+ Minnesota Vikings    | Mike Zimmer
+(8 rows)
+
+#6
+-- The 50 players with the highest salaries
+
+SELECT name FROM players ORDER BY salary DESC LIMIT 50;
+          name
+-------------------------
+ Peyton Manning
+ Drew Brees
+ Dwight Freeney
+ Elvis Dumervil
+ Michael Vick
+ Sam Bradford
+ Jared Allen
+ Matt Ryan
+ Matthew Stafford
+ Tamba Hali
+ Jake Long
+ Nnamdi Asomugha
+ Trent Williams
+ Vincent Jackson
+
+ #7
+ nfl=# SELECT AVG (salary) FROM players;
+         avg
+----------------------
+ 1579692.539817232376
+(1 row)
+
+# 8
+nfl=# 
+SELECT name, position FROM players WHERE salary < 1000000;
+          name          | position
+------------------------+----------
+ Kirk Morrison          | LB
+ Corey McIntyre         | RB
+ C.J. Spiller           | RB
+ Garrison Sanborn       | LB
+ Eric Wood
